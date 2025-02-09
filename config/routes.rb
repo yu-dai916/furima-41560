e3b53c7a3b0ee9c8cd772322c0b6bdfe40d39613
yu-dai916 <respectyou0916@icloud.com>
2025-02-09 09:49:55 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   get 'posts', to: 'posts#index'
+    root to: "application#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
